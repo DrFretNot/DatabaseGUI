725259d47e4857d241e0fee71349cfaab85cfe7f
DrFretNot <trentford@Trents-MacBook-Air.local>
2016-02-18 16:02:21 -0600

    Basic database reading implemented
    The Ok button will now read the first (0 customerid) entry

--- GuiExample/src/guiexample/MainWindow.java
+++ GuiExample/src/guiexample/MainWindow.java
@@ -6,6 +6,7 @@
 package guiexample;
 
 import javax.swing.JOptionPane;
+import java.sql.*;
 
 /**
  *
@@ -262,8 +263,73 @@ public class MainWindow extends javax.swing.JFrame {
 
     private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
         String msg = "Hello, " + firstNameTextField.getText();
-  
         JOptionPane.showMessageDialog(this, msg);
+        
+        try {
+            // load and register the JDBC driver
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Driver could not be loaded. Exiting.");
+            System.exit(1);
+        }
+        
+         // create a connection to the database
+        try (Connection conn =
+                DriverManager.getConnection(
+                    "jdbc:hsqldb:file:sampledata", "SA", ""))
+        {
+            // Work with the database
+            System.out.println("Connection opened successfully!");
+            
+            // Create the statement object
+            Statement st = conn.createStatement();
+            
+            // Execute the SQL query
+            ResultSet rs = st.executeQuery("SELECT * FROM Customers WHERE CustomerId = 0;");
+            
+            //System.out.println("<?xml version=\"1.0\"/>");
+            //System.out.println("<customers>");
+            
+            // Loop through the rows
+            while (rs.next()) {
+                // Process one row of the data
+                int id = rs.getInt("CUSTOMERID");
+                String fName = rs.getString("FIRSTNAME");
+                String lName = rs.getString("LASTNAME");
+                Date dob = rs.getDate("DOB");
+                customerIdTextField.setText(""+ id);
+                firstNameTextField.setText(fName);
+                lastNameTextField.setText(lName);
+                cityTextField.setText(rs.getString("CITY"));
+                stateTextField.setText(rs.getString("STATE"));
+                phoneNumberTextField.setText(rs.getString("PHONENUMBER"));
+                joinDateTextField.setText(rs.getString("JOINDATE"));
+                dobTextField.setText(rs.getString("DOB"));
+                /*
+                System.out.println(
+                        "  <customer>\n" +
+                        "    <customerid>" + (id + 1) + "</customerid>\n" +
+                        "    <firstname>" + fName + "</firstname>\n" +
+                        "    <lastname>" + lName + "</lastname>\n" +
+                        "    <birthdate>\n" +
+                        "      <year>" + dob.getYear() + "</year>\n" +
+                        "      <month>" + dob.getMonth() + "</month>\n" +
+                        "      <day>" + dob.getDay() + "</day>\n" +
+                        "    </birthdate>\n" +
+                        "  </customer>");*/
+            }
+            
+            //System.out.println("</customers>");
+            
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.err.println("Something went wrong. :-(");
+            System.exit(1);
+        }
+        
+        
+        
     }//GEN-LAST:event_okButtonActionPerformed
 
     private void dobTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dobTextFieldActionPerformed
